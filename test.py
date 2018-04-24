176533e17eb85d35a6ede0e46053cd209c7234c8
DuncanNewzoo <duncan@newzoo.com>
2018-04-24 09:45:46 +0200

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class TestSomething():
+
+    def test_false(self):
+        assert True == False
+        return
+
+    def test_true(self):
+        assert True == True
+        return
